b544ca263f85a110563cb8df80b621908aea63f2
ErikMaung <infiniteerik123@gmail.com>
2024-09-10 17:44:11 -0700

    restyle and rename fun

--- src/App.js
+++ src/App.js
@@ -11,18 +11,26 @@ import Speaker from './components/Speaker';
 import Game from './Game';
 
 function App() {
-  const [isFun, setIsFun] = useState(false);
+  const [isGame, setIsGame] = useState(false);
   const [scaleValue, setScaleValue] = useState(1.1);
-  const toggleFun = () => {
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  const toggleGame = () => {
     setScaleValue(scaleValue === 1.1 ? 1.5 : 1.1);
-    setIsFun(!isFun);
+    setIsGame(!isGame);
+    if (!isGame) {
+      setShowOverlay(true);
+      setTimeout(() => {
+        setShowOverlay(false);
+      }, 2500); // 2.5 sec
+    }
   };
 
   return (
     <MantineProvider className='root'>
-      <div className={`fade-container ${isFun ? 'active' : ''}`}>
+      <div className={`fade-container ${isGame ? 'active' : ''}`}>
         <Draggable>
-          <Speaker isFun={isFun} />
+          <Speaker isGame={isGame} />
         </Draggable>
       </div>
       <Title className='warning'>
@@ -34,15 +42,20 @@ function App() {
         </Title>
         <div className="button-group">
           <CustomButton label="Home" type={"disabled"} scaleValue={1.0} />
-          <CustomButton label="Portfolio" type={isFun ? "primary" : "secondary"} href={"https://erikmaung.github.io/portfolio"} scaleValue={scaleValue} />
-          <CustomButton label={isFun ? "Fun: ON" : "Fun: OFF"} type={isFun ? "success" : "secondary"} scaleValue={scaleValue} onClick={toggleFun} />
+          <CustomButton label="Portfolio" type={isGame ? "primary" : "secondary"} href={"https://erikmaung.github.io/portfolio"} scaleValue={scaleValue} />
+          <CustomButton label={isGame ? "Game: ON" : "Game: OFF"} type={isGame ? "success" : "secondary"} scaleValue={scaleValue} onClick={toggleGame} />
         </div>
         <AnimatedList />
       </Container>
       <Container className={`experiment rotate`} />
-      <div className={`fade-container ${isFun ? 'active' : ''}`}>
+      <div className={`fade-container ${isGame ? 'active' : ''}`}>
         <Game scaleValue={scaleValue} />
       </div>
+      
+        <div className={`overlay-message ${showOverlay ? '' : 'hide'}`}>
+          Scroll to the bottom to play the game!
+        </div>
+      
       <Container className="footer">
         &copy; Erik Maung 2024
       </Container>
